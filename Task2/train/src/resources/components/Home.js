7ac197bcf3907c8339e98c56a63d3d2ed06b5ccf
siddarths1 <siddarths.20cse@kongu.edu>
2023-07-26 11:39:42 +0530

    0.2

--- Task2/train/src/resources/components/Home.js
+++ Task2/train/src/resources/components/Home.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Home() {
+    useEffect(async()=>{
+       await axios.post("http://20.244.56.144/train/auth",{
+        companyName: "Train",
+        clientID: "676d7987-3864-437e-9ef0-d2e7920fa98d",
+        clientSecret: "DosQwekMuIpfDbtm",
+        ownerName: "siddarths",
+        ownerEmail: "siddarths.20cse@kongu.edu",
+        rollNo: "20CSR198"
+    }).then((res)=>{
+        console.log(res);
+    }).catch((err)=>{
+        console.log(err);
+    })
+    },[])
   return (
     <div>Home</div>
   )
