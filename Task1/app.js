59de04f75ff13d62b915a61ee943c6aa450394ab
siddarths1 <siddarths.20cse@kongu.edu>
2023-07-26 10:25:45 +0530

    0.2

--- Task1/app.js
+++ Task1/app.js
@@ -8,10 +8,18 @@ app.get("/",(req,res)=>{
 })
 
 app.get("/numbers",async(req,res)=>{
+    const q=req.query;
+    const result=new set();
     
+
+    console.log(q);
+    res.send("working");
 })
 
 
 app.listen(8008,()=>{
     console.log("Listening")
 })
+
+
+// http://localhost:8008/numbers?url=http://20.244.56.144/numbers/primes&url=http://abc.com/fibo
