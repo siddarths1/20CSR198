08a867671804549f1bf352a97a66834936496b08
siddarths1 <siddarths.20cse@kongu.edu>
2023-07-26 11:28:43 +0530

    0.2

--- Task2/train/src/App.js
+++ Task2/train/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './resources/components/Home';
+import Train from './resources/components/Train';
 function App() {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/train" element={<Train/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ Task2/train/src/resources/components/Train.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Train() {
+  return (
+    <div>Train</div>
+  )
+}
