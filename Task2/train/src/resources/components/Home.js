dcb1ce26ad296420f7ae13e396a008d881fd492e
siddarths1 <siddarths.20cse@kongu.edu>
2023-07-26 11:44:31 +0530

    0.5

--- Task2/train/src/resources/components/Home.js
+++ Task2/train/src/resources/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-
+import axios from "axios";
 export default function Home() {
     useEffect(async()=>{
        await axios.post("http://20.244.56.144/train/auth",{
@@ -14,6 +14,11 @@ export default function Home() {
     }).catch((err)=>{
         console.log(err);
     })
+    // await axios.get("http://20.244.56.144/train/trains").then((res)=>{
+    //     console.log(res);
+    // }).catch((err)=>{
+    //     console.log(err);
+    // })
     },[])
   return (
     <div>Home</div>
