cce3b184328e6c2317d7d1e745ac05f2b659813b
siddarths1 <siddarths.20cse@kongu.edu>
2023-07-26 11:34:48 +0530

    0.3

--- Task2/train/src/App.js
+++ Task2/train/src/App.js
@@ -16,3 +16,12 @@ function App() {
 }
 
 export default App;
+
+// {
+//   "companyName": "Train",
+//   "clientID": "676d7987-3864-437e-9ef0-d2e7920fa98d",
+//   "clientSecret": "DosQwekMuIpfDbtm",
+//   "ownerName": "siddarths",
+//   "ownerEmail": "siddarths.20cse@kongu.edu",
+//   "rollNo": "20CSR198"
+// }
