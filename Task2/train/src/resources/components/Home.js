61e7726111906e15060bf34248dea28445ed5b3f
siddarths1 <siddarths.20cse@kongu.edu>
2023-07-26 12:02:45 +0530

    0.1

--- Task2/train/src/resources/components/Home.js
+++ Task2/train/src/resources/components/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
+import Card from 'react-bootstrap/Card';
 export default function Home() {
 const [token,setToken]=useState();
-
+const[data,setData]=useState([]);
     useEffect(async()=>{
        await axios.post("http://20.244.56.144/train/auth",{
         companyName: "Train",
@@ -11,23 +12,43 @@ const [token,setToken]=useState();
         ownerName: "siddarths",
         ownerEmail: "siddarths.20cse@kongu.edu",
         rollNo: "20CSR198"
-    }).then((res)=>{
+    }).then(async(res)=>{
         
         setToken(res.data.access_token);
-    }).catch((err)=>{
-        console.log(err);
-    })
-    await axios.get("http://20.244.56.144/train/trains",{
+        await axios.get("http://20.244.56.144/train/trains",{
         headers: {
-          'Authorization': `Bearer ${token}`
+          'Authorization': `Bearer ${res.data.access_token}`
         }
       }).then((res)=>{
-        console.log(res);
+
+        console.log(res.data);
+        setData(res.data);
     }).catch((err)=>{
         console.log(err);
     })
+    }).catch((err)=>{
+        console.log(err);
+    })
+    
     },[])
   return (
-    <div>Home</div>
+    <>
+    <Container></Container>
+    {
+     data.map((items,index)=>{
+     return (<Card key={index} style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{items.trainName}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{items.trainNumber}</Card.Subtitle>
+        <Card.Text>
+            Seats Sleeper : {items.seatsAvailable.sleeper} Price : {items.price.sleeper}<br/>
+            Seats AC : {items.seatsAvailable.AC} Price : {items.price.AC} <br/>
+        </Card.Text>
+        <Card.Link href={`/train?id=${items.trainNumber}`}>Check</Card.Link>
+      </Card.Body>
+    </Card>)
+    })
+}
+    </>
   )
 }
